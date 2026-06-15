787128db846ce4714ed14978ec974c4b95c76519
qianxuntudou-ops <qianxuntudou@gmail.com>
2026-06-15 20:27:45 +0800

    v1.5.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,8 +53,8 @@ android {
         applicationId = "com.tutu.myblbl"
         minSdk = 23
         targetSdk = 35
-        versionCode = 55
-        versionName = "1.5.5"
+        versionCode = 56
+        versionName = "1.5.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
